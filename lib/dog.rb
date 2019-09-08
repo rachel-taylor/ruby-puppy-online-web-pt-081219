02ed3ea090a5ca640401d3ba652db083a2345a44
Rachel T <rachmaybe@gmail.com>
2019-09-08 02:15:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,35 @@ class Dog
   @@all = []
   @@names = []
   
+   attr_accessor :name
+
   def initialize(name)
-    @name=name
-  end 
-  
-  def name 
-    @name
-  end 
+    #binding.pry
+    @name = name
+    @@names << self.name
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    names_list = [ ]
+    @@names.each do |thing|
+      if names_list.include?(thing) == false
+        names_list << "#{thing}"
+      end
+    end
+  puts names_list
+end
+
+  def self.clear_all
+    @@all.clear
+    @@names.clear
+  end
 end 
